f000b1946db51837833532f924baf78dfa79ad1d
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-27 22:21:04 +0700

    Update FlappyBall.js

--- FlappyBall.js
+++ FlappyBall.js
@@ -8,3 +8,18 @@ const JUMP_HEIGHT = 9.0;
 const GROUND_HEIGHT = 20; 
 const WIDTH = 400;
 const HEIGHT = 600;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    for (i=0;i<n;i++){
+        xdir= Math.pow(-1,Math.floor(Math.random()*100));
+        ydir= Math.pow(-1,Math.floor(Math.random()*100));
+        p.push(new SmallBall(random(350),random(350),14,1,xdir,ydir));
+        p[i].speed = i % 5 + 1;
+    }
+}
+
+//menberi angka random integer
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
